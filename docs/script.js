cd0b3b442d589b2bfdbe5a4d70ab15e16120c742
ErikAlvesAlmeida <eriikalves55@gmail.com>
2025-09-24 11:28:33 -0300

    update site

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.menu-btn');
-const sidebar = document.querySelector('.sidebar');
-
-btn.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-});
